56a995b293cc9caa758b6fe6d3138ac9d03848ad
declan-tyson <declan.j.tyson@gmail.com>
2021-12-16 10:59:40 +0000

    all musts undet the MoSCoW convention have been met

--- propertyListingWebsite/index.js
+++ propertyListingWebsite/index.js
@@ -53,24 +53,28 @@ const getProperties = () => {
 
                 const propertyCard = document.createElement("div");
                 propertyCard.classList.add("card");
+                
+
+                const propertyBody = document.createElement("div");
+                propertyBody.classList.add("card-body");
 
 
 
                 const propertyContainer = document.createElement("div");
                 //propertyBody.classList.add("card-body");
 
-                const address = document.createElement("h5");
-                address.innerText = `Address: ${property.address}`;
-                propertyContainer.appendChild(address);
+                const address = document.createElement("h4");
+                address.innerText = `${property.address}`;
+                propertyBody.appendChild(address);
 
-                const location = document.createElement("p");
+                const location = document.createElement("h4");
                 location.innerText = `${property.location}`;
-                propertyContainer.appendChild(location);
+                propertyBody.appendChild(location);
 
 
                 const price = document.createElement("p");
                 price.innerText = `£${property.price}`;
-                propertyContainer.appendChild(price);
+                propertyBody.appendChild(price);
 
                 const bedrooms = document.createElement("p");
                 if (`${property.bedrooms}`== 0) {
@@ -82,16 +86,19 @@ const getProperties = () => {
                 else {
                     bedrooms.innerText = `${property.bedrooms} Bedrooms`;
                 }
-                propertyContainer.appendChild(bedrooms);
+                propertyBody.appendChild(bedrooms);
 
-                const gpsLocation = `${property.gps_location}`;
-                const id = `${property.id}`;
-                let gpsValues = new Array();
-                gpsValues = gpsLocation.split(",");
+                
+
+                // const gpsLocation = `${property.gps_location}`;
+                // const id = `${property.id}`;
+                // let gpsValues = new Array();
+                // gpsValues = gpsLocation.split(",");
+                // propertyCard.appendChild(gpsLocation);
 
                 const propertyDelete= document.createElement("button");
                 propertyDelete.innerText ="Remove";
-                propertyContainer.appendChild(propertyDelete);
+                
                 propertyDelete.addEventListener("click",function(){
                     axios.delete(`http://localhost:8080/remove/${property.id}`)
                     .then(res => {
@@ -101,10 +108,14 @@ const getProperties = () => {
                     })
                     .catch(err => console.error(err));
                 })
+                propertyBody.appendChild(propertyDelete);
+                propertyCard.appendChild(propertyBody);
+                
+            
+
 
                 
-
-
+                getOutput.appendChild(propertyCard);
                 getOutput.appendChild(propertyContainer);
                 /*insert google map here */
 
@@ -112,7 +123,29 @@ const getProperties = () => {
             }
         })
         .catch(err => console.log("error", err));
-}
+    }
+
+document.querySelector("#updateProperty").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const data = {
+        id: this.id.value,
+        address: this.address.value,
+        gps_location: this.gps_location.value,
+        location: this.location.value,
+        price: this.price.value,
+        bedrooms: this.bedrooms.value
+    };
+
+    axios
+        .put(`http://localhost:8080/replace/${data.id}`, data)
+        .then(res => {
+           
+            this.reset();
+            this.id.focus();
+            console.log(res);
+            getProperties();
+        })
+        .catch(err => console.error(err));
+});
 getProperties();
-
-
